b796fa4e7e7340ed81a8693cf5e80d5f352ee62c
theamanman-sys <theamanman-sys@users.noreply.github.com>
2026-05-12 14:05:29 +0300

    Track Firebase config for production deploy

---
+++ js/env-config.js
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyArOatx6mi-loTY5YkUHZONEzR5CYUIZ4A",
+  authDomain: "domiinique-db.firebaseapp.com",
+  projectId: "domiinique-db",
+  storageBucket: "domiinique-db.firebasestorage.app",
+  messagingSenderId: "979202038788",
+  appId: "1:979202038788:web:61f9055a91f7f2da52a951",
+  measurementId: "G-BV91XC3N3R"
+};
